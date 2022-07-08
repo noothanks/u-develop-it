4d516a2e0e50dc44764acea0eb946d7970f0fb2b
noothanks <coreyfuller727@yahoo.com>
2022-07-08 13:47:29 -0400

    creating server connection

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//default for request not found
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+//starting server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
